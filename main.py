6c5dcda0a2ca14302ae1b6a226fa27c828313b3b
AlesyaLaz <alesya.lazerko@icloud.com>
2022-04-05 17:12:16 +0300

    adding URL Repositories

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+import requests as r
+import json as j
+
+url = 'https://api.github.com/users/Faddey32/repos'
+
+response = r.get(url)
+response_json = response.json()
+
+for repo in response_json:
+    print(f'Repo:{repo["name"]}, URL: {repo["html_url"]}')
